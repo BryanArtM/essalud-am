6df760d70d47611f5437f81da42ff2c8b81e6344
Bryan <barteagame@gmail.com>
2025-06-17 11:05:18 -0500

    make models

---
+++ app/Models/AdultoMayor.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class AdultoMayor extends Model
+{
+    use HasFactory;
+
+    public function enfermedades() {
+        return $this->hasOne(Enfermedad::class);
+    }
+
+    public function riesgos() {
+        return $this->hasOne(Riesgo::class);
+    }
+
+    public function evaluaciones() {
+        return $this->hasMany(Evaluacion::class);
+    }
+
+    public function citas() {
+        return $this->hasMany(Cita::class);
+    }
+
+    public function tratamientos() {
+        return $this->hasMany(Tratamiento::class);
+    }
+
+    public function evaluacionGeriatrica() {
+        return $this->hasOne(EvaluacionGeriatrica::class);
+    }
+
+    public function valoraciones() {
+        return $this->hasMany(Valoracion::class);
+    }
+
+        public function actividadEducativa() {
+        return $this->hasMany(ActividadEducativa::class);
+    }
+
+}
+
---
+++ app/Models/Riesgo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class Riesgo extends Model
+{
+    use HasFactory;
+
+    public function adultoMayor() {
+        return $this->belongsTo(AdultoMayor::class);
+    }
+
+}
